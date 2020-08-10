d1944d768f4ae57c0ed55c96787469a7a04c3120
Erin Hughes <Erin.Hughes@ibm.com>
2020-08-10 13:56:36 +0100

    Updates

---
+++ precache-manifest.e0cb47c45e0dc05101dbaf04b74d9879.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2e791d7469c39c581cce09dc5481b1",
+    "url": "/fabric-website/index.html"
+  },
+  {
+    "revision": "e0df3582dabf4cfaee48",
+    "url": "/fabric-website/static/css/main.26b76d49.chunk.css"
+  },
+  {
+    "revision": "26e8c30b62b5ca4edecb",
+    "url": "/fabric-website/static/js/2.5550a2d0.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/fabric-website/static/js/2.5550a2d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0df3582dabf4cfaee48",
+    "url": "/fabric-website/static/js/main.c0ce4c8f.chunk.js"
+  },
+  {
+    "revision": "a8f685dfad87f33abd11",
+    "url": "/fabric-website/static/js/runtime-main.51367635.js"
+  },
+  {
+    "revision": "dffd1d5d4665892df3a642250aa344c7",
+    "url": "/fabric-website/static/media/Caliper.dffd1d5d.png"
+  },
+  {
+    "revision": "dd2503aa13c05e5f8111360e53656482",
+    "url": "/fabric-website/static/media/Fabric website header.dd2503aa.png"
+  },
+  {
+    "revision": "348da4589bb4246c15c26d57a70428cc",
+    "url": "/fabric-website/static/media/Fabric website illustration.348da458.png"
+  },
+  {
+    "revision": "cd011f694dc152e499491914b29cfa5a",
+    "url": "/fabric-website/static/media/arrow--right.cd011f69.svg"
+  },
+  {
+    "revision": "a2c135d5ad97168d41c0fe4a2e096a7d",
+    "url": "/fabric-website/static/media/caret--down.a2c135d5.svg"
+  },
+  {
+    "revision": "aa086bdf6d89680593f8be765db4a631",
+    "url": "/fabric-website/static/media/caret--up.aa086bdf.svg"
+  },
+  {
+    "revision": "8f3a14036a333eb5ec9f45a8897b2668",
+    "url": "/fabric-website/static/media/go.8f3a1403.png"
+  },
+  {
+    "revision": "703cfe5ddb3c73e52bd01a63a28dcab0",
+    "url": "/fabric-website/static/media/launch.703cfe5d.svg"
+  },
+  {
+    "revision": "06dff72556517cb92e607ca1a892d26e",
+    "url": "/fabric-website/static/media/nodejs.06dff725.png"
+  }
+]);
